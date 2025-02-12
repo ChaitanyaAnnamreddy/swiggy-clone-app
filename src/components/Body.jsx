b9ddca4b9e63a4770fae05c1f15548bcefef553c
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-02-12 19:17:07 +0530

    api change

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -13,7 +13,7 @@ const Body = ({ searchText }) => {
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
+      `https://instafood.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999`
     )
 
     const json = await data.json()
