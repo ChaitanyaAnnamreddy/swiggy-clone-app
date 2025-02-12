c024a13e861b15d9c94698f96657196c456acbd8
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-02-13 01:22:20 +0530

    css fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,9 @@ const Footer = () => {
     <footer className="bg-white dark:bg-gray-900">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0 md:flex md:justify-between">
-            <a href="/" className="flex items-center">
-              <img
-                src={logo}
-                className="h-8 me-3"
-                alt="FlowBite Logo"
-              />
+          <div className="mb-6 md:mb-0 md:flex md:justify-between footer-logo">
+            <a href="/" className="flex items-center footer-logo">
+              <img src={logo} className="h-8 me-3" alt="FlowBite Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Swiggy Clone App
               </span>
